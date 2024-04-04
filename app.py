551fd28c51c1115cf03e269eba784f65e98a1278
Rishabh Saini <113230008+SainiRishabh01@users.noreply.github.com>
2024-04-04 18:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ model = pickle.load(open('model.pkl','rb'))
 
 st.title("Thyroid Disease Prediction using ML")
 
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3= st.columns(3)
 
 with col1:
     fo = st.text_input('Age')
@@ -17,40 +17,40 @@ with col2:
 with col3:
     flo = st.text_input('whether patient is on thyroxine(t or f)')
 
-with col4:
+with col1:
     Jitter_percent = st.text_input('query_on_thyroxine(t or f)')
 
-with col5:
+with col2:
     Jitter_Abs = st.text_input('whether patient is on antithyroid medication(t or f)')
 
-with col1:
+with col3:
     RAP = st.text_input('whether patient is sick(t or f)')
 
-with col2:
+with col1:
     PPQ = st.text_input('Patient is pregnant(t or f)')
 
-with col3:
+with col2:
     DDP = st.text_input('whether patient has undergone thyroid surgery(t or f)')
 
-with col4:
+with col3:
     Shimmer = st.text_input('whether patient is undergoing Iodine 131 treatment(t or f)')
 
-with col5:
+with col1:
     Shimmer_dB = st.text_input('whether patient believes they have hypothyroid(t or f)')
 
-with col1:
+with col2:
     APQ3 = st.text_input('whether patient believes they have hyperthyroid(t or f)')
 
-with col2:
+with col3:
     APQ5 = st.text_input('whether patient  lithium(t or f)')
 
-with col3:
+with col1:
     APQ = st.text_input('whether patient has goitre(t or f)')
 
-with col4:
+with col2:
     DDA = st.text_input('whether patient has tumor(t or f)')
 
-with col5:
+with col3:
     NHR = st.text_input('whether patient has hypopituitary(t or f)')
 
 with col1:
@@ -62,34 +62,34 @@ with col2:
 with col3:
     DFA = st.text_input('TSH')
 
-with col4:
+with col1:
     spread1 = st.text_input('T3_measured(t or f)')
 
-with col5:
+with col2:
     spread2 = st.text_input('T3')
 
-with col1:
+with col3:
     D2 = st.text_input('TT4_measured(t or f)')
 
-with col2:
+with col1:
     PPE = st.text_input('TT4')
 
-with col3:
+with col2:
     PPE1 = st.text_input('T4U_measured(t or f)')
 
-with col4:
+with col3:
     PPE2 = st.text_input('T4U')
 
-with col5:
+with col1:
     PPE3 = st.text_input('FTI_measured(t or f)')
 
-with col1:
+with col2:
     PPE4 = st.text_input('FTI')
 
-with col2:
+with col3:
     PPE5 = st.text_input('TBG_measured(t or f)')
 
-with col3:
+with col1:
     PPE6 = st.text_input('TBG')
 
 # code for Prediction
